86a50f5edee8ab7c63eb0344cfd16c1b48468828
Mateusz Rychel <mateusz.rychel@outlook.com>
2025-06-17 14:35:33 +0200

    Added interface for UploadPage

--- frontend/src/routes/Upload.tsx
+++ frontend/src/routes/Upload.tsx
@@ -1,24 +1,26 @@
 import { useState } from "react";
 
 const Upload = () => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [value, setValue] = useState(0);
+
+  const [isSent, setIsSent] = useState(false);
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100 dark:bg-gray-800 text-black dark:text-white">
-    <img
-        src="/src/asetts/IMG_20250617_121752980_HDR.jpg"
-        alt="Obraz"
-        className="w-full max-w-md rounded-lg shadow-md mt-4"
-      />
+    
+    <input type="file" className=""></input>
+
+    <input type="range" min="0" max="1" step="0.1" value={value} onChange={(e) => setValue(Number(e.target.value))}></input>
+    <p className="mt-2 text-gray-700">Wartość: {value}</p>
+    
 
   <button
-    onClick={() => setIsVisible(!isVisible)}
-    className="w-full max-w-md py-2 mt-4 bg-blue-500 text-white rounded hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-800"
-  >
-    Podgląd
+    onClick={() => setIsSent(!isSent)}
+    className="w-full max-w-md py-2 mt-4 bg-blue-500 text-white rounded hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-800">
+    Prześlij
   </button>
 
-  {isVisible && <h4></h4>}
+  {isSent && <h4></h4>}
 </div>
 
   );
